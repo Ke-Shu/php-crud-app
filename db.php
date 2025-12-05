2459beb9e2bb69f2ef6702811809e4d7c8c41912
Ke-Shu <javkelbubb@gmail.com>
2025-12-04 22:19:22 -0400

    live host

--- db.php
+++ db.php
@@ -1,12 +1,16 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "crud_app";
 
+$host = "sql100.infinityfree.com";
+$user = "iF0_40602448";
+$pass = "pheonixK1ng" your infinityFree password
+$db   = "iF0_40602448_crud_app";
+
+// Create connection
 $conn = new mysqli($host, $user, $pass, $db);
 
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
 ?>
